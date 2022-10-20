e48bc826491594d7ddbd267151331c996a3a74b2
kanaka1234 <gtkanaka6@gmail.com>
2022-10-20 12:37:11 +0530

    second

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-import logging
+from flask import Flask, request
+import sys
+
+import pip
+from housing.util.util import read_yaml_file, write_yaml_file
+from matplotlib.style import context
 from housing.logger import logging
-from flask import Flask
-app=Flask(__name__)
-@app.route("/",methods=['GET','POST'])
+from housing.exception import HousingException
+import os, sys
+import json
+from housing.config.configuration import Configuartion
+from housing.constant import CONFIG_DIR, get_current_time_stamp
+from housing.pipeline.pipeline import Pipeline
+from housing.entity.housing_predictor import HousingPredictor, HousingData
+from flask import send_file, abort, render_template
+
+
+ROOT_DIR = os.getcwd()
+LOG_FOLDER_NAME = "logs"
+PIPELINE_FOLDER_NAME = "housing"
+SAVED_MODELS_DIR_NAME = "saved_models"
+MODEL_CONFIG_FILE_PATH = os.path.join(ROOT_DIR, CONFIG_DIR, "model.yaml")
+LOG_DIR = os.path.join(ROOT_DIR, LOG_FOLDER_NAME)
+PIPELINE_DIR = os.path.join(ROOT_DIR, PIPELINE_FOLDER_NAME)
+MODEL_DIR = os.path.join(ROOT_DIR, SAVED_MODELS_DIR_NAME)
+
+
+from housing.logger import get_log_dataframe
+
+HOUSING_DATA_KEY = "housing_data"
+MEDIAN_HOUSING_VALUE_KEY = "median_house_value"
+
+app = Flask(__name__)
+
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    logging.info("hello")
-    return "MMmmMy first project got deployed"
-if __name__=="__main__":
-    app.run(debug=True)
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        return str(e)
